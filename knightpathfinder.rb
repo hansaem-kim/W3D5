7e1c432329c281a59bd6577e32c15fbc5323a73e
James <jameschen625@gmail.com>
2021-07-02 16:17:13 -0400

    Finished KnightPathFinder.rb

--- knightpathfinder.rb
+++ knightpathfinder.rb
@@ -46,8 +46,24 @@ class KnightPathFinder
         end
     end
 
+    def trace_path_back(node)
+        moves = []
+        current = node
+        until current.parent.nil?
+            moves << current.value
+            current = current.parent
+        end
+        moves << current.value
+        moves.reverse
+    end
+
+    def find_path(end_pos)
+        end_node = @root_node.bfs(end_pos)
+        trace_path_back(end_node)
+    end
+
 end
 
-kfs = KnightPathFinder.new([4,4])
-
-p kfs
+kpf = KnightPathFinder.new([0, 0])
+p kpf.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
+p kpf.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
